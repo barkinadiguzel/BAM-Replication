701f92fc1c8653b02d4f7a33eeb29a6f03a3e155
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-30 00:51:34 +0300

    Update bam_model.py

--- src/models/bam_model.py
+++ src/models/bam_model.py
@@ -2,23 +2,58 @@ import torch
 import torch.nn as nn
 
 from blocks.bam_blocks import BAMBlock
-from config import REDUCTION_RATIO, DILATION, IN_CHANNELS, NUM_BAM_BLOCKS
+from config import REDUCTION_RATIO, DILATION
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class BAMStage(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = ConvBlock(out_channels, out_channels)
+
+        self.bam = BAMBlock(
+            in_channels=out_channels,
+            reduction_ratio=REDUCTION_RATIO,
+            dilation=DILATION
+        )
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.bam(x)      
+        x = self.pool(x)
+        return x
 
 
 class BAMModel(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=3):
         super().__init__()
 
-        self.bam_blocks = nn.ModuleList([
-            BAMBlock(
-                in_channels=IN_CHANNELS,
-                reduction_ratio=REDUCTION_RATIO,
-                dilation=DILATION
-            )
-            for _ in range(NUM_BAM_BLOCKS)
-        ])
+        self.stage1 = BAMStage(in_channels, 64)
+        self.stage2 = BAMStage(64, 128)
+        self.stage3 = BAMStage(128, 256)
+        self.stage4 = BAMStage(256, 512)
 
     def forward(self, x):
-        for bam in self.bam_blocks:
-            x = bam(x)
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
         return x
